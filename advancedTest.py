7ba908f4884c834ba17d4ed838c308dade5fb150
George <george_artikis@protonmail.com>
2024-03-05 22:11:49 +0000

    Changed formula for new medication score and removed paremeters from existing score

--- advancedTest.py
+++ advancedTest.py
@@ -5,12 +5,12 @@ effects_data = {'new_medication': [], 'existing_medication': []}
 
 
 # Simulate the effect of medications on blood pressure
-def new_medication(age, weight, baseline_bp):
-    # New medication effectiveness depends on age and baseline blood pressure
-    return baseline_bp - (10 + age / 10 + np.random.normal(0, 5))
+def new_medication_score(age, weight, baseline_bp):
+    # New medication effectiveness depends on age, weight in kg and baseline blood pressure in bpm
+    return baseline_bp - (10 + (age / 10) + (weight / 5) + np.random.normal(0, 5))
 
 
-def existing_medication(age, weight, baseline_bp):
+def existing_medication_score(baseline_bp):
     # Existing medication effectiveness is constant
     return baseline_bp - (15 + np.random.normal(0, 5))
 
@@ -18,7 +18,7 @@ def existing_medication(age, weight, baseline_bp):
 # Strategy for generating patient profiles
 patient_profile = st.fixed_dictionaries({
     'age': st.integers(18, 80),
-    'weight': st.floats(50.0, 150.0),
+    'weight': st.floats(50.0, 200.0),
     'baseline_bp': st.floats(120.0, 180.0)
 })
 
@@ -27,8 +27,8 @@ patient_profile = st.fixed_dictionaries({
 @given(patient_profile)
 def test_medication_effectiveness(patient):
     age, weight, baseline_bp = patient['age'], patient['weight'], patient['baseline_bp']
-    new_bp = new_medication(age, weight, baseline_bp)
-    existing_bp = existing_medication(age, weight, baseline_bp)
+    new_bp = new_medication_score(age, weight, baseline_bp)
+    existing_bp = existing_medication_score(age, weight, baseline_bp)
     effects_data['new_medication'].append(baseline_bp - new_bp)
     effects_data['existing_medication'].append(baseline_bp - existing_bp)
 
